922787702214e0f75fe94424a713336cd6ea0937
Тамара Солодун <solodun.t@donnu.edu.ua>
2020-09-23 19:31:20 +0300

    Task_1 done

---
+++ Task_1/Task_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Task_1
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Привіт, ДонНУ!");
+        }
+    }
+}
