5a6a3d919c887c7b01d8025dcde42a1f44da4b7d
Тамара Солодун <solodun.t@donnu.edu.ua>
2020-09-23 19:38:01 +0300

    Task_2 and Task_3 added

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Task_2
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+            Console.WriteLine("Введіть змінну a типу int: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть змінну b типу double: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть змінну c типу long: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($" a = {a}; b = {b}; c = {c}");
+        }
+    }
+}
---
+++ Task_3/Task_3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Task_3
+    {
+        static void Main(string[] args)
+        {
+            bool check = true;
+
+            while (check)
+            {
+                try
+                {                   
+                    Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+                    Console.WriteLine("Введіть змінну a типу int: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введіть змінну b типу double: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Введіть змінну c типу long: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+
+                    Console.WriteLine($" a = {a}; b = {b}; c = {c}");
+
+                    check = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Введіть коректну змінну!");
+                   
+                }
+            }
+        }
+    }
+}
